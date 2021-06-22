38e6df872d36bc9077284fb3b427aed517164af3
Gyana1997 <gyana1997rinku@gmail.com>
2021-06-22 21:28:19 +0530

    delete unnecessary file

--- src/basic/Checkbox.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Checkbox = ({ input, label, type, meta, className, disabled, initialValue, onChange, meta: { touched, error } }) => (
-  <span style={{ border: touched && error ? "1px solid red" : "none" }}>
-    <input type="checkbox" className={className} {...input} />
-    <label className="form-check-label labelclass">{label}</label>
-  </span>
-)
